ef8b270178b849470e543e1f64f47b12888d7bce
Nabil <nabilfairbairn@gmail.com>
2025-04-25 17:15:11 -0400

    default current date

--- script.js
+++ script.js
@@ -41,6 +41,24 @@ function sortActivitiesByScoreDesc() {
 
 sortActivitiesByScoreDesc()
 
+window.addEventListener("load", function() {
+  var now = new Date();
+  var utcString = now.toISOString().substring(0,19);
+  var year = now.getFullYear();
+  var month = now.getMonth() + 1;
+  var day = now.getDate();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  var localDatetime = year + "-" +
+                    (month < 10 ? "0" + month.toString() : month) + "-" +
+                    (day < 10 ? "0" + day.toString() : day) + "T" +
+                    (hour < 10 ? "0" + hour.toString() : hour) + ":" +
+                    (minute < 10 ? "0" + minute.toString() : minute)
+  var datetimeField = document.getElementById("event-date");
+  datetimeField.value = localDatetime;
+});
+
 function current_date_hours_minutes() {
   const now = new Date();
 
